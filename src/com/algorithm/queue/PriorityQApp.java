0f90537f6e0c77996c1da15614daaf01f2b43778
wangzhenya <zywang@ciilabs.org>
2014-12-03 17:50:23 +0800

    queue

---
+++ src/com/algorithm/queue/PriorityQApp.java
@@ -0,0 +1,23 @@
+package com.algorithm.queue;
+
+import java.io.IOException;
+
+public class PriorityQApp {
+	
+	public static void main(String[] args) throws IOException {
+		PriorityQ thePQ = new PriorityQ(5);
+		thePQ.insert(30);
+		thePQ.insert(50);
+		thePQ.insert(10);
+		thePQ.insert(40);
+		thePQ.insert(20);
+		
+		while(!thePQ.isEmpty()) {
+			long item = thePQ.remove();
+			System.out.print(item + "  ");
+		}
+		
+		System.out.println(" ");
+
+	}
+}
